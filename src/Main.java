1764e47166f0bd4942c4066a8acd6905a2e8cc39
Gustavo <reis.gustavoqueiroz@gmail.com>
2022-10-19 12:00:38 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -10,18 +9,47 @@ public class Main {
         curso1.setTitulo("curso java");
         curso1.setDescricao("descrição curso java");
         curso1.setCargaHoraria(8);
-        System.out.println(curso1);
 
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("mentoria curso java");
         mentoria1.setDescricao("descrição mentoria java");
         mentoria1.setData(LocalDate.now());
-        System.out.println(mentoria1);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("curso HTML");
         curso2.setDescricao("descrição curso HTML");
         curso2.setCargaHoraria(5);
-        System.out.println(curso2);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devGustavo = new Dev();
+        devGustavo.setNome("Gustavo");
+        devGustavo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Gustavo: " + devGustavo.getConteudosInscritos());
+        devGustavo.progredir();
+        devGustavo.progredir();
+        System.out.println("--");
+        System.out.println("Conteúdos inscritos Gustavo: " + devGustavo.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Gustavo: " + devGustavo.getConteudosConcluidos());
+        System.out.println("XP: " + devGustavo.calcularTotalXp());
+
+        System.out.println("--");
+
+        Dev devAlice = new Dev();
+        devAlice.setNome("Alice");
+        devAlice.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Alice: " + devAlice.getConteudosInscritos());
+        devAlice.progredir();
+        devAlice.progredir();
+        devAlice.progredir();
+        System.out.println("--");
+        System.out.println("Conteúdos inscritos Alice: " + devAlice.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Alice: " + devAlice.getConteudosConcluidos());
+        System.out.println("XP: " + devAlice.calcularTotalXp());
     }
 }
